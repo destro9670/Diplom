bfa8c9a489a9efe8cb75fc156be1ff3c988cd26c
destr <dm.gabz@gmail.com>
2020-04-11 01:29:48 +0300

    add one TODO in SeverStarter about console control

--- src/main/java/ServerStarter.java
+++ src/main/java/ServerStarter.java
@@ -8,7 +8,7 @@ import java.net.ServerSocket;
 public class ServerStarter {
 
     private static final Logger logger = Logger.getLogger(ServerStarter.class);
-
+    ///TODO(1) normal console control
     public static void main(String[] args) {
         try(ServerSocket ss = new ServerSocket(5222)) {
             ConnectionListener listener = new ConnectionListener();
